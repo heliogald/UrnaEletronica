42bc73439d176d71b5751c2a0f694b0ed2c6b1ff
Hélio Galdino da Silva <heliogald@hotmail.com>
2025-04-13 16:54:21 -0300

    feat: adção/modificação de cadidatos.

--- Repositories/CandidatoRepository.cs
+++ Repositories/CandidatoRepository.cs
@@ -8,7 +8,10 @@ namespace UrnaEletronica.Repositories
         private readonly List<Candidato> _candidatos = new List<Candidato>
         {
             new Candidato { Numero = "10", Nome = "Homer Simpson", Partido = "ABC" },
-            new Candidato { Numero = "20", Nome = "Sr Barnes", Partido = "DEF" }            
+            new Candidato { Numero = "20", Nome = "Sr Barnes", Partido = "DEF" },
+            new Candidato { Numero = "30", Nome = "Picapau", Partido = "GHI" },
+            new Candidato { Numero = "40", Nome = "Leoncio", Partido = "JLM" }
+
         };
 
         public Candidato BuscarPorNumero(string numero)
